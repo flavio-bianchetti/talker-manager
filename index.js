de8c1fa8515e3627f49380f37d350ab6f7deac5a
Flávio Bianchetti <fjb.developer@gmail.com>
2022-02-15 16:28:00 -0300

    Add: Requirement 1 completed

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+const fs = require('fs').promises;
+
+app.get('/talker', async (req, res) => {
+  fs.readFile('./talker.json', 'utf8')
+    .then((data) => {
+      const response = JSON.parse(data);
+      if (!response || response.length === 0) {
+        return res.status(200).send([]);
+      }
+      return res.status(200).json(response);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
